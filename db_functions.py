cbb3d4a5b91e33831918962e82273f513d58668d
nsaef <ns@ella.ag>
2021-01-09 12:40:40 +0100

    added notes to db_functions.py

--- db_functions.py
+++ db_functions.py
@@ -16,9 +16,13 @@ import TwitterAPI
 import pickle
 import API_KEYS
 
+
 # v.1.0
 
-def df_to_sql(df, tablename, drop=False):
+
+# TODO: Dynamically typed languages allow a value that can be either bool or string, but it's not very clean
+#  it would be better to make it a string but assign "None" as the default value and check "if not drop/if drop is None"
+def df_to_sql(df: pd.DataFrame, tablename: str, drop=False):
     """
     Writes Dataframe to DB
     :param df: name of dataframe that will be written to DB
@@ -32,16 +36,18 @@ def df_to_sql(df, tablename, drop=False):
     elif drop == False:
         df.to_sql(tablename, con=engine)
     elif drop == "append":
-        df.to_sql(tablename, con=engine, index = False, if_exists='append')
+        df.to_sql(tablename, con=engine, index=False, if_exists='append')
     else:
-        print ("Error raised by df_to_sql(): None of the write conditions (replace, drop, append) was met. Check function parameters!")
+        print("Error raised by df_to_sql(): None of the write conditions (replace, drop, append) was met. "
+              "Check function parameters!")
         sys.exit()
+    # TODO: Should probably be a function call?
     engine.dispose
 
 
-def init_db_connections():
+def init_db_connections() -> tuple:
     """
-    Convienience function so I dont have to call sql_alchemy_engine() and  db_connect() separately
+    Convenience function so I dont have to call sql_alchemy_engine() and db_connect() separately
     :return: engine, connection, metadata
     """
     engine, metadata = sql_alchemy_engine()
@@ -49,18 +55,18 @@ def init_db_connections():
     return engine, connection, metadata
 
 
-def sql_alchemy_engine():
-    '''
-    Replace engine_sting with code following below example
+def sql_alchemy_engine() -> tuple:
+    """
+    Replace engine_string with code following the example below
     engine = create_engine('postgresql://postgres:YOUR_PASSWORD@localhost:5433/tw_lytics01')
     :return: engine, metadata
-    '''
+    """
     engine = create_engine(API_KEYS.engine_string)
     metadata = MetaData(engine)
     return engine, metadata
 
 
-def staging_timestamp():
+def staging_timestamp() -> datetime.timestamp:
     """
     Creates Timestamp in format "%Y%m%d_%H%M"
     :return: timestamp
@@ -71,7 +77,7 @@ def staging_timestamp():
 
 
 def db_connect():
-    '''
+    """
     Replace connection_string with code following below example:
         connection = psycopg2.connect(user="postgres",
                                   password="YOUR_PASSWORD",
@@ -80,28 +86,29 @@ def db_connect():
                                   database="YOUR_DB_NAME")
     Hint: often it's port 5432
     :return: psycopg2 connection
-    '''
+    """
     try:
-
         connection = API_KEYS.connection_string
         cursor = connection.cursor()
         # Print PostgreSQL Connection properties
         # print ( connection.get_dsn_parameters(),"\n")
-
+        
         # Print PostgreSQL version
         cursor.execute("SELECT version();")
-        record = cursor.fetchone()
+        # record = cursor.fetchone()
         # print("You are connected to - ", record,"\n")
-
+    
     except (Exception, psycopg2.Error) as error:
         print("Error while connecting to PostgreSQL", str(error))
+        # TODO: Handle variable connection in case of error
     return connection
 
 
 def db_close(connection):
     try:
         # closing database connection.
-        if (connection):
+        if connection:
+            # TODO: Variable cursor isn't defined
             cursor.close()
             connection.close()
             print("PostgreSQL connection is closed")
@@ -110,6 +117,7 @@ def db_close(connection):
         # print ("It seems no DB connection not was open.")
 
 
+# TODO: Remove deprecated parameters
 def drop_table(name, engine=0, metadata=0):
     """
     :param name: Table name to be dropped
@@ -119,50 +127,63 @@ def drop_table(name, engine=0, metadata=0):
     engine, metadata = sql_alchemy_engine()
     table_to_drop = Table(name, metadata)
     table_to_drop.drop(engine, checkfirst=True)
-
+    
     # df_5.to_sql('isert_test01', con=engine)  # writes df to Database using SQL alchemy engine
 
 
-def select_from_db(sql):
-    '''
+def select_from_db(sql) -> pd.DataFrame:
+    """
     Sends an SQL Statement to the DB
     :param sql: SQL Statement that will be executed by the DB
     :return: Dataframe with Query Result
-    '''
+    """
     connection = db_connect()
-    df = sqlio.read_sql_query(sql, connection)  # stagin table
+    df = sqlio.read_sql_query(sql, connection)  # staging table
     db_close(connection)
     return df
 
 
+# TODO: Missing docstring
 def update_table(sql):
     connection = db_connect()
     cursor = connection.cursor()
-    print (sql)
+    print(sql)
     cursor.execute(sql)
     connection.commit()
     cursor.close()
     db_close(connection)
-    print("Update statement send: " + sql)
+    print("Update statement sent: " + sql)
+
 
 def update_to_invalid(cur_date, user_id):
     """
-    used to set columns to invalid in n_users. These columns are not used anymore during next run. Used for non german tweets and tweets of deletes users
+    Used to set columns to invalid in table n_users. These columns are not used anymore during next run.
+    Used for non-German tweets and tweets of deleted users
     :return:
     """
-    sql_invalid = f"update n_users set lr = 'invalid', pol = 'invalid', lr_pol_last_analysed = '{cur_date}' where id  = {user_id}"
+    sql_invalid = f"update n_users set lr = 'invalid', pol = 'invalid', lr_pol_last_analysed = '{cur_date}' " \
+                  f"where id  = {user_id}"
     update_table(sql_invalid)
     print(f"Set {user_id} to invalid")
 
 
-def get_staging_table_name(table_name):
+# TODO: Missing docstring
+def get_staging_table_name(table_name: str) -> str:
     return 's_h_' + table_name + '_' + str(staging_timestamp())
 
-def create_empty_staging_table(table_name):
-    sql = 'CREATE TABLE {} (   index bigint,    id bigint,    conversation_id bigint,    created_at text COLLATE pg_catalog."default",    date text COLLATE pg_catalog."default",     tweet text COLLATE pg_catalog."default",    hashtags text COLLATE pg_catalog."default",    user_id bigint,    username text COLLATE pg_catalog."default",    name text COLLATE pg_catalog."default",    link bigint,    retweet bigint,    nlikes bigint,    nreplies bigint,    nretweets bigint,    quote_url bigint,    user_rt_id bigint,    user_rt bigint,    staging_name text COLLATE pg_catalog."default")'.format(table_name)
+
+# TODO: Missing docstring
+def create_empty_staging_table(table_name: str) -> None:
+    sql = f'CREATE TABLE {table_name} (   index bigint,    id bigint,    conversation_id bigint,    created_at text ' \
+          'COLLATE pg_catalog."default",    date text COLLATE pg_catalog."default",     tweet text COLLATE ' \
+          'pg_catalog."default",    hashtags text COLLATE pg_catalog."default",    user_id bigint,    username text ' \
+          'COLLATE pg_catalog."default",    name text COLLATE pg_catalog."default",    link bigint,    ' \
+          'retweet bigint,    nlikes bigint,    nreplies bigint,    nretweets bigint,    quote_url bigint,    ' \
+          'user_rt_id bigint,    user_rt bigint,    staging_name text COLLATE pg_catalog."default")'
     update_table(sql)
 
-def save_pickle(df, filename):
+
+def save_pickle(df: pd.DataFrame, filename: str) -> None:
     """
     :param filename: filename to load
     :return: none
@@ -171,7 +192,7 @@ def save_pickle(df, filename):
         pickle.dump(df, file)
 
 
-def load_pickle(filename):
+def load_pickle(filename: str):
     """
     :param filename: filename to laod
     :return: loaded file
